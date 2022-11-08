d62385c070825d7241613a230f64d2889f0ed0a9
Marina Papapetrou <contact@marinapapap.com>
2022-11-08 14:31:02 +0000

    ex 2 counter

---
+++ excercise-2/spec/counter_spec.rb
@@ -0,0 +1,12 @@
+require 'counter'
+
+RSpec.describe Counter do
+
+    it "adds a count to the number and prints the count" do
+      counter = Counter.new
+      counter.add(1)
+      result = counter.report
+      expect(result).to eq "Counted to 1 so far."
+    end
+    
+end
